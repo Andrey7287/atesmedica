ee652c2eb72a208d31c6f77cea26d31bdf15af6d
Андрей С. Алексеев <a.alekseev@infodesign.local>
2016-07-01 16:55:04 +0300

    html ready

---
+++ js/project.js
@@ -0,0 +1,9 @@
+var slider = $('.slider').slick({
+  autoplay: true,
+  dots: true
+});
+slider.on('afterChange', function(event, slick, currentSlide, nextSlide){
+
+  //console.log(currentSlide)
+
+});
