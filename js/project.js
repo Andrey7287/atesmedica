9aa062c5250a677d3b7242044943d77ec21c3c52
Андрей С. Алексеев <a.alekseev@INFODESIGN.LOCAL>
2016-07-05 17:51:16 +0300

    aside

--- js/project.js
+++ js/project.js
@@ -1,28 +1,28 @@
 define(['jquery', 'slick', 'bootstrap'], function(){
 
-	/*SLIDER*/
-	var slider = $('.slider').slick({
-		autoplay: false,
-		dots: true
-	});
-	slider.on('afterChange', function(event, slick, currentSlide, nextSlide){
+  /*SLIDER*/
+  var slider = $('.slider').slick({
+    autoplay: false,
+    dots: true
+  });
+  slider.on('afterChange', function(event, slick, currentSlide, nextSlide){
 
-		//console.log(currentSlide)
+    //console.log(currentSlide)
 
-	});
+  });
 
-	/*menu*/
-	
-	var mq = window.matchMedia( "(min-width: 768px)" );
-	if (mq.matches) {
-		 
-		$('#dropdown').hover(function(e){
-			$(this).click();
-		});
-		
-	}
-	
-	// ADD SLIDEDOWN ANIMATION TO DROPDOWN //
+  /*menu*/
+
+  var mq = window.matchMedia( "(min-width: 768px)" );
+  if (mq.matches) {
+
+    $('.dropdown').hover(function(){
+      $(this).find('.dropdown-menu').stop().slideToggle();
+    });
+
+  }
+
+  // ADD SLIDEDOWN ANIMATION TO DROPDOWN //
   $('.dropdown').on('show.bs.dropdown', function(e){
     $(this).find('.dropdown-menu').first().stop(true, true).slideDown();
   });
@@ -31,8 +31,22 @@ define(['jquery', 'slick', 'bootstrap'], function(){
   $('.dropdown').on('hide.bs.dropdown', function(e){
     $(this).find('.dropdown-menu').first().stop(true, true).slideUp();
   });
-	
-	
+
+
+
+  $('.dropdown-menu').add('.site-nav__item').hover(function(){
+
+      if( $(this).children('.site-nav__link').length ) {
+        $(this).children('.site-nav__link').toggleClass('hovered');
+      } else {
+        $(this).siblings('.site-nav__link').hover(function(){
+          $(this).toggleClass('hovered');
+        });
+      }
+
+    }
+  );
+
 });
 
 
