45a06d906790ee0bcfa888231d824a59a4a4166b
Андрей С. Алексеев <a.alekseev@INFODESIGN.LOCAL>
2016-07-06 17:36:58 +0300

    add pr

--- js/project.js
+++ js/project.js
@@ -1,14 +1,46 @@
 define(['jquery', 'slick', 'bootstrap'], function(){
 
+  $.fn.ravno = function () {
+    var maxH = -1;
+    var $cols = $(this).height("auto").each(function () {
+      var h = $(this).height();
+      maxH = (h > maxH) ? h : maxH;
+    });
+    $cols.height(maxH);
+  };
+
+  $('.ravno').ravno();
+
+  /* Navigator slide munus */
+  function ToggleSlide(e){
+
+    $('.slider').slick('slickPause');
+
+    if ( $(e.target).attr('data-toggle') === "menu" ) {
+      $(e.target).find('.navigator__title').addClass('navigator__title--closed');
+      $(e.target).find('.navigator__slide-block').slideDown();
+    } else if ( $(e.target).attr('data-toggle') === "slider" ||  $(e.target).parent().attr('data-toggle') === "slider") {
+      e.preventDefault();
+      var $sliderButtons = $('.slick-dots').find('button');
+      var targetNum = $(e.target).attr('data-num');
+      $sliderButtons[targetNum].click();
+    } else if ( $(e.target).attr('data-toggle') === "production" ){
+      var $current
+    }
+  };
+  $('#navigator-wrap').on('click', '.navigator', ToggleSlide);
+
   /*SLIDER*/
   var slider = $('.slider').slick({
-    autoplay: false,
+    autoplay: true,
     dots: true
   });
   slider.on('afterChange', function(event, slick, currentSlide, nextSlide){
 
-    //console.log(currentSlide)
-
+    //console.log($navigator[currentSlide])
+    var navigators = $('#navigator-wrap').find('.navigator');
+    $(navigators).removeClass('active');
+    $(navigators[currentSlide]).addClass('active');
   });
 
   /*menu*/
